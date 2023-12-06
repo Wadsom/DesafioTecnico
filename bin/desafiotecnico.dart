05ea163daae221a8da448d7b5baff9b5c829aec5
Wadson <pinheirowadson@gmail.com>
2023-12-06 01:20:49 -0300

    .

--- bin/desafiotecnico.dart
+++ bin/desafiotecnico.dart
@@ -3,6 +3,10 @@ import './funcaoDesafio/index.dart';
 void main() {
   try {
     //Informe o valor inteiro desejado dentro do parametro de entrada da função.
+
+    // int result = calculate(n: 20);
+    // print(result);
+    
     print(calculate(n: 10));
   } catch (e) {
     //Caso informe um número negativo, um alerta sera acionado.
